d97c0d99cf4e541b629dc819e35ebfed1e1e9dc8
heineiuo <heineiuo@gmail.com>
2018-05-28 15:26:33 +0800

    implement a new skiplist

--- src/Skiplist.js
+++ src/Skiplist.js
@@ -9,8 +9,8 @@ import bufferEqual from 'buffer-equal'
  * @param {*} b 
  */
 function isEqual(a, b) {
-  if (!(Buffer.isBuffer(a) && Buffer.isBuffer(b))) return a === b
-  return bufferEqual(a, b)
+  if (!(Buffer.isBuffer(a) && Buffer.isBuffer(b))) return a === b;
+  return bufferEqual(a, b);
 }
 
 /**
@@ -34,10 +34,6 @@ function makeNode(level, key, value) {
 function nodesEqual(left, right) {
   if ((left === undefined) && right) return false;
   if ((right === undefined) && left) return false;
-  // if (left[0] !== right[0]) return false;
-  // if (left[1] !== right[1]) return false;
-  // if (left[2] !== right[2]) return false;
-  // if (left[3] !== right[3]) return false;
   if (!isEqual(left[0], right[0])) return false;
   if (!isEqual(left[1], right[1])) return false;
   if (!isEqual(left[2], right[2])) return false;
@@ -54,7 +50,7 @@ class Skiplist {
     this.maxlevel = Math.round(Math.log(this.maxsize, 2));
 
     this.level = 0;
-    this.head = makeNode(this.maxlevel);
+    this.head = makeNode(this.maxlevel); // head在左下角
     this.tail = NIL;
     for (let i = 0; i < this.maxlevel; i++) {
       this.head[i + 3] = NIL;
@@ -66,6 +62,12 @@ class Skiplist {
     }
   }
 
+  /**
+   * 返回一个小于等于(this.level + 1)的level
+   * 
+   * 从概率上来讲偏小的level可能性更大
+   * 
+   */
   _randomLevel() {
     let lvl = 0;
     const max = Math.min(this.maxlevel, this.level + 1);
@@ -75,6 +77,11 @@ class Skiplist {
     return lvl;
   }
 
+  /**
+   * 
+   * @param {*} search 
+   * @param {boolean} reverse 
+   */
   find(search, reverse) {
     let node = reverse ? this.tail : this.head[3];
     const idx = reverse ? 2 : 3;
@@ -94,6 +101,12 @@ class Skiplist {
     return results;
   }
 
+  /**
+   * 
+   * @param {*} search 
+   * @param {boolean} maxResultsToReturn 
+   * @param {boolean} reverse 
+   */
   findWithCount(search, maxResultsToReturn, reverse) {
     let node = reverse ? this.tail : this.head[3];
     const idx = reverse ? 2 : 3;
@@ -112,26 +125,34 @@ class Skiplist {
     return results;
   };
 
+  /**
+   * 从head节点开始，如果右边的节点有值，则计数
+   */
   length() {
     // more for my curiosity
     let node = this.head[3];
     let count = 0;
-    while (node[0]) {
+    while (node[0]) { 
       count++;
       node = node[3];
     }
     return count;
   };
 
+  /**
+   * 
+   * @param {number[]} update 
+   * @param {string|Buffer} search 
+   */
   _findLess(update, search) {
     let node = this.head;
     for (let i = this.level; i >= 0; i--) {
-      let key = node[3 + i][0];
+      let key = node[3 + i][0]; // 首先是head里最上层的第一个节点，然后是
       while (key && (key < search)) {
-        node = node[3 + i];
+        node = node[3 + i]; // 下面的节点
         key = node[3 + i] ? node[3 + i][0] : null;
       }
-      update[i] = node;
+      update[i] = node; // 如果key不存在或者在search右边， 更新update[i]为node
     }
     return node;
   };
@@ -141,14 +162,14 @@ class Skiplist {
     const update = this._update.slice(0);
     let node = this._findLess(update, key);
     const prev = node;
-    node = node[3];
+    node = node[3]; // 右节点
     if (isEqual(node[0], key)) {
       node[1] = value;
     } else {
       const lvl = this._randomLevel();
       this.level = Math.max(this.level, lvl);
       node = makeNode(lvl, key, value);
-      node[2] = prev;
+      node[2] = prev; // 左节点
       for (let i = 0; i <= this.level; i++) {
         node[3 + i] = update[i][3 + i];
         update[i][3 + i] = node;
@@ -189,6 +210,11 @@ class Skiplist {
     return false; // just to make it explicit
   };
 
+  /**
+   * 从head的最高的一个level开始往下找
+   * 如果右节点的key存在且key比待查找的小，则往右边找，
+   * @param {string|Buffer} search 
+   */
   match(search) {
     let node = this.head;
     for (let i = this.level; i >= 0; i--) {
