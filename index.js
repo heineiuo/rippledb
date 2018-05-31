6e75680bbe5cd5e8370e17f8a7ee7c9deafee598
heineiuo <heineiuo@gmail.com>
2018-06-01 00:10:48 +0800

    add copyright comments

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2018-present, heineiuo.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+
 'use strict';
 
 module.exports = module.exports.default = require('./dist/Database').default;
