5b61e750423cbb92f0056ba31ff33808a57cada9
heineiuo <heineiuo@gmail.com>
2019-07-29 11:12:01 +0800

    Create BitBuffer

---
+++ src/BitBuffer.js
@@ -0,0 +1,43 @@
+// https://github.com/wiedi/node-bitbuffer
+
+import { Buffer } from 'buffer'
+
+/**
+ * Buffer length should be Math.ceil(bits / 8)
+ */
+export default class BitBuffer {
+  constructor (buffer) {
+    this._buffer = buffer
+    this._size = buffer.length
+  }
+
+  get buffer ():Buffer {
+    return this._buffer
+  }
+
+  get size ():number {
+    return this._size
+  }
+
+  get bits ():number {
+    // return (this.size - (this.size % 8)) * 8
+    return this.size * 8
+  }
+
+  set = (index:number, bool:boolean) => {
+    const pos = index >>> 3
+    if (bool) {
+      this._buffer[pos] |= 1 << (index % 8)
+    } else {
+      this._buffer[pos] &= ~(1 << (index % 8))
+    }
+  }
+
+  toggle (index:number) {
+    this._buffer[index >>> 3] ^= 1 << (index % 8)
+  }
+
+  get = (index:number):boolean => {
+    return (this._buffer[index >>> 3] & (1 << (index % 8))) !== 0
+  }
+}
