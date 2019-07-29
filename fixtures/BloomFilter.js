38e75b0027f7ad1b9a954f2cb65282646216a954
heineiuo <heineiuo@gmail.com>
2019-07-29 15:38:39 +0800

    Update

--- fixtures/BloomFilter.js
+++ fixtures/BloomFilter.js
@@ -5,12 +5,12 @@ console.log('filter bits: ', filter._bitBuffer.toString())
 
 console.log('kNumber: ', filter.kNumber)
 const keys = ['a', 'a1']
-console.log('keys: ', keys.join(', '))
+console.log('put keys: ', keys.join(', '))
 filter.putKeys(keys, keys.length)
 console.log('filter bits: ', filter._bitBuffer.toString())
 
 const filter2 = new BloomFilter(filter.buffer)
 
-console.log('a: ', filter2.keyMayMatch('a'))
-console.log('a1: ', filter2.keyMayMatch('a1'))
-console.log('xxx: ', filter2.keyMayMatch('xxx'))
+console.log('test key a: ', filter2.keyMayMatch('a'))
+console.log('test key a1: ', filter2.keyMayMatch('a1'))
+console.log('test key xxx: ', filter2.keyMayMatch('xxx'))
