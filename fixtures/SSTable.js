f906ecded3a7eb193d80e3009ad51cc5700c1759
heineiuo <heineiuo@gmail.com>
2019-08-02 23:57:27 +0800

    Update

--- fixtures/SSTable.js
+++ fixtures/SSTable.js
@@ -1,6 +1,13 @@
 const fs = require('fs').promises
+const path = require('path')
 const SSTable = require('../build/SSTable').default
+const SSTableFooter = require('../build/SSTableFooter').default
 
-const table = new SSTable()
+async function main () {
+  const ldbPath = path.resolve(__dirname, '../.db/0001.ldb')
+  const buf = await fs.readFile(ldbPath)
+  const footer = new SSTableFooter(buf)
+  console.log(footer.get())
+}
 
-table.add()
+main()
