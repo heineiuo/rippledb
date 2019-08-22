8d2bfbc3864356703266d9e3149dc5d8e59cbeee
heineiuo <heineiuo@gmail.com>
2019-08-22 10:59:15 +0800

    Update slice

--- fixtures/slice.js
+++ fixtures/slice.js
@@ -5,6 +5,11 @@ function main () {
   addProperty(slice, 'x', 'y')
   addProperty(slice, 'x1', 'y1')
   console.log(slice)
+
+  const slice2 = new Slice(slice)
+  console.log(slice2)
+  addProperty(slice2, 'x3', 'y3')
+  console.log(slice)
 }
 
 function addProperty(slice, propertyName, value){
