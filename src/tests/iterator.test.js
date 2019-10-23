fe64d0ac8aadd6f9983deeea9268a66b8305c375
heineiuo <heineiuo@gmail.com>
2019-10-23 17:31:14 +0800

    fix: iterator got deleted key (#63)
    * fix: iterator got deleted key
    
    * Update iterator test

--- src/tests/iterator.test.js
+++ src/tests/iterator.test.js
@@ -10,12 +10,30 @@ afterAll(() => {
 describe('Database Iterator', () => {
   test('iterator with start option', async done => {
     const db = new Database(dbpath)
+    let cacheKey = null
     for (let i = 0; i < 1000; i++) {
-      await db.put(...random())
+      const entry = random()
+      if (i === 500) cacheKey = entry[0]
+      await db.put(...entry)
     }
 
-    for await (const entry of db.iterator({ start: 'fff' })) {
-      expect(entry.key.compare(Buffer.from('fff')) > 0).toBe(true)
+    let count = 0
+    let cacheKey2 = null
+    for await (const entry of db.iterator({ start: cacheKey })) {
+      if (count === 0) {
+        cacheKey2 = `${entry.key}`
+      }
+      expect(entry.key.compare(Buffer.from(cacheKey)) > 0).toBe(true)
+      count++
+      if (count > 10) break
+    }
+
+    await db.del(cacheKey2)
+    count = 0
+    for await (const entry of db.iterator({ start: cacheKey })) {
+      expect(entry.key.compare(Buffer.from(cacheKey2)) !== 0).toBe(true)
+      count++
+      if (count > 10) break
     }
     done()
   })
