5e3dbcde723c0ab709f561908cdc2321adc17518
heineiuo <heineiuo@gmail.com>
2019-11-12 12:44:25 +0800

    fix test

--- src/tests/lock.test.ts
+++ src/tests/lock.test.ts
@@ -1,9 +1,6 @@
 import Database from '../Database'
 import { createDir, cleanup } from '../../fixtures/dbpath'
 import { Options } from '../Options'
-import { StatusError } from '../Status'
-
-jest.setTimeout(60000 * 10)
 
 const dbpath = createDir()
 afterAll(() => {
@@ -12,11 +9,12 @@ afterAll(() => {
 
 cleanup(dbpath)
 
-test('lock', async () => {
+test('lock', async done => {
   const options = new Options()
   options.debug = true
   const db1 = new Database(dbpath)
   expect(await db1.ok()).toBe(true)
   const db2 = new Database(dbpath, options)
   await expect(db2.ok()).rejects.toThrowError(/EEXIST/)
+  done()
 })
