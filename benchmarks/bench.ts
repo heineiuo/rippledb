b13e50f60eaf57eaa328b8990ba51f9fd5915aac
heineiuo <heineiuo@gmail.com>
2019-11-25 10:28:42 +0800

    Add benchmark script (#101)
    * Add bench script
    * Avoid create log writer conflict

--- benchmarks/bench.ts
+++ benchmarks/bench.ts
@@ -1,1 +1,67 @@
-console.log('bench')
+import { Database } from '../build'
+import { random } from '../fixtures/random'
+import { createDir, cleanup } from '../fixtures/dbpath'
+import fs from 'fs'
+import path from 'path'
+import { argv } from 'yargs'
+
+function now(): number {
+  return Number(process.hrtime.bigint()) / Math.pow(10, 6)
+}
+
+async function runner(
+  db: Database,
+  dataset: [string, string][],
+  skip: number,
+  start: number
+): Promise<void> {
+  let current = start
+  const total = dataset.length
+  while (true) {
+    if (current >= total) return
+    const entry = dataset[current]
+    await db.put(entry[0], entry[1])
+    current += skip
+  }
+}
+
+async function bench(total: number, runnerCount: number): Promise<void> {
+  const dataset = []
+  for (let i = 0; i < total; i++) {
+    dataset.push([random(16), random(100)])
+  }
+
+  const dbpath = createDir('bench')
+  cleanup(dbpath)
+  const db = new Database(dbpath)
+
+  const startTime = now()
+  await Promise.all(
+    Array.from({ length: runnerCount }, (v, start) => {
+      return runner(db, dataset, runnerCount, start)
+    })
+  )
+
+  const endTime = now()
+  const totalTime = endTime - startTime
+
+  const file = await fs.promises.open(
+    path.resolve(__dirname, '../bench.log'),
+    'a+'
+  )
+  const log = `
+time    : ${new Date().toISOString()}
+key     : 16 bytes
+value   : 100 bytes
+total   : ${total}
+runners : ${runnerCount} 
+speed   : ${totalTime.toFixed(2)} ms total; ${(
+    (totalTime / total) *
+    1000
+  ).toFixed(2)} us/op
+`
+  console.log(log)
+  await file.appendFile(log)
+}
+
+bench(parseInt(argv.total as string), parseInt(argv.runner as string))
