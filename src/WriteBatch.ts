18a5333ec83b33d0c31a8d7c1cc3a6f23b2ff58e
heineiuo <heineiuo@gmail.com>
2019-11-26 13:53:27 +0800

    Improve WriteBatch performance

--- src/WriteBatch.ts
+++ src/WriteBatch.ts
@@ -13,14 +13,12 @@ import LogRecord from './LogRecord'
 import { SequenceNumber, EntryRequireType, ValueType } from './Format'
 import { decodeFixed64, encodeFixed32, decodeFixed32 } from './Coding'
 
-// TODO should be separate to two class: WriteBatch and WriteBatchInternal
-
 export class WriteBatchInternal {
   // WriteBatch header has an 8-byte sequence number followed by a 4-byte count.
   static kHeader = 12
 
   static byteSize(batch: WriteBatch): number {
-    return batch.buffer.length - WriteBatchInternal.kHeader
+    return batch.buffers.reduce((size, buf) => size + buf.length, 0)
   }
 
   static insert(batch: WriteBatch, mem: MemTable): void {
@@ -33,29 +31,30 @@ export class WriteBatchInternal {
   }
 
   static getContents(batch: WriteBatch): Buffer {
-    return batch.buffer
+    return Buffer.concat([batch.head, ...batch.buffers])
   }
 
   static setContents(batch: WriteBatch, contents: Buffer): void {
     assert(contents.length >= WriteBatchInternal.kHeader)
-    batch.buffer = contents
+    batch.head = contents.slice(0, this.kHeader)
+    batch.buffers = [contents.slice(this.kHeader)]
   }
 
   // sequence must be lastSequence + 1
   static setSequence(batch: WriteBatch, sequence: number): void {
-    batch.buffer.fill(new SequenceNumber(sequence).toFixed64Buffer(), 0, 7)
+    batch.head.fill(new SequenceNumber(sequence).toFixed64Buffer(), 0, 7)
   }
 
   static getSequence(batch: WriteBatch): SequenceNumber {
-    return new SequenceNumber(decodeFixed64(batch.buffer.slice(0, 8)))
+    return new SequenceNumber(decodeFixed64(batch.head.slice(0, 8)))
   }
 
   static setCount(batch: WriteBatch, count: number): void {
-    batch.buffer.fill(encodeFixed32(count), 8, 11)
+    batch.head.fill(encodeFixed32(count), 8, 11)
   }
 
   static getCount(batch: WriteBatch): number {
-    return decodeFixed32(batch.buffer.slice(8, 12))
+    return decodeFixed32(batch.head.slice(8, 12))
   }
 
   static append(dst: WriteBatch, src: WriteBatch): void {
@@ -63,11 +62,10 @@ export class WriteBatchInternal {
       dst,
       WriteBatchInternal.getCount(src) + WriteBatchInternal.getCount(dst)
     )
-    assert(src.buffer.length >= WriteBatchInternal.kHeader)
-    dst.buffer = Buffer.concat([
-      dst.buffer,
-      src.buffer.slice(WriteBatchInternal.kHeader),
-    ])
+    // for (const buf of src.buffers) {
+    //   dst.buffers.push(buf)
+    // }
+    dst.buffers = dst.buffers.concat(src.buffers)
   }
 }
 
@@ -83,64 +81,62 @@ export class WriteBatchInternal {
 //    len: varint32
 //    data: uint8[len]
 export class WriteBatch {
-  get buffer(): Buffer {
-    return this._buffer
-  }
-
-  set buffer(value: Buffer) {
-    this._buffer = value
-  }
-
-  constructor() {
-    this._buffer = Buffer.alloc(WriteBatchInternal.kHeader)
-  }
-
-  private _buffer!: Buffer
+  buffers: Buffer[] = []
+  head: Buffer = Buffer.alloc(WriteBatchInternal.kHeader)
 
   put(key: string | Buffer, value: string | Buffer): void {
     const record = LogRecord.add(new Slice(key), new Slice(value))
-    this.buffer = Buffer.concat([this.buffer, record.buffer])
+    this.buffers.push(record.buffer)
     WriteBatchInternal.setCount(this, WriteBatchInternal.getCount(this) + 1)
   }
 
   del(key: string | Buffer): void {
     const record = LogRecord.del(new Slice(key))
-    this.buffer = Buffer.concat([this.buffer, record.buffer])
+    this.buffers.push(record.buffer)
     WriteBatchInternal.setCount(this, WriteBatchInternal.getCount(this) + 1)
   }
 
   clear(): void {
-    this._buffer = Buffer.alloc(WriteBatchInternal.kHeader)
+    this.buffers = []
+    this.head = Buffer.alloc(WriteBatchInternal.kHeader)
   }
 
   *iterator(): IterableIterator<EntryRequireType> {
-    let index = WriteBatchInternal.kHeader
-    while (index < this.buffer.length) {
-      const valueType = this.buffer.readUInt8(index)
-      index++
-      const keyLength = varint.decode(this.buffer, index)
-      index += varint.decode.bytes
-      const keyBuffer = this.buffer.slice(index, index + keyLength)
-      index += keyLength
+    let buffersIndex = 0
+    const buffersCount = this.buffers.length
+    while (buffersIndex < buffersCount) {
+      const buffer = this.buffers[buffersIndex]
+      const bufferLength = buffer.length
+      let index = 0
 
-      if (valueType === ValueType.kTypeDeletion) {
+      while (index < bufferLength) {
+        const valueType = buffer.readUInt8(index)
+        index++
+        const keyLength = varint.decode(buffer, index)
+        index += varint.decode.bytes
+        const keyBuffer = buffer.slice(index, index + keyLength)
+        index += keyLength
+
+        if (valueType === ValueType.kTypeDeletion) {
+          yield {
+            type: valueType,
+            key: new Slice(keyBuffer),
+            value: new Slice(),
+          }
+          continue
+        }
+
+        const valueLength = varint.decode(buffer, index)
+        index += varint.decode.bytes
+        const valueBuffer = buffer.slice(index, index + valueLength)
+        index += valueLength
         yield {
           type: valueType,
           key: new Slice(keyBuffer),
-          value: new Slice(),
+          value: new Slice(valueBuffer),
         }
-        continue
       }
-
-      const valueLength = varint.decode(this.buffer, index)
-      index += varint.decode.bytes
-      const valueBuffer = this.buffer.slice(index, index + valueLength)
-      index += valueLength
-      yield {
-        type: valueType,
-        key: new Slice(keyBuffer),
-        value: new Slice(valueBuffer),
-      }
+      buffersIndex++
     }
   }
 }
