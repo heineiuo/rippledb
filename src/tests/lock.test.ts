5d727c336109cafba3113130d249e21a5343f840
heineiuo <heineiuo@gmail.com>
2019-12-08 00:41:54 +0800

    Add lockfile stale (#107)
    * Add lockfile stale
    
    * Add basic destroy

--- src/tests/lock.test.ts
+++ src/tests/lock.test.ts
@@ -1,10 +1,15 @@
 import Database from '../Database'
 import { createDir, cleanup } from '../../fixtures/dbpath'
 import { Options } from '../Options'
+import { copydb } from '../../fixtures/copydb'
+
+jest.setTimeout(60000 * 10)
 
 const dbpath = createDir()
+const dbpath3 = createDir()
 afterAll(() => {
   cleanup(dbpath)
+  cleanup(dbpath3)
 })
 
 cleanup(dbpath)
@@ -16,5 +21,8 @@ test('lock', async done => {
   expect(await db1.ok()).toBe(true)
   const db2 = new Database(dbpath, options)
   await expect(db2.ok()).rejects.toThrowError(/EEXIST/)
+  await copydb(dbpath, dbpath3)
+  const db3 = new Database(dbpath3, options)
+  await expect(db3.ok()).resolves.toBe(true)
   done()
 })
