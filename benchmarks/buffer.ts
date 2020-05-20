a2e74f2ba3e9f6ebea487cf19a4d6d5a05950543
heineiuo <heineiuo@gmail.com>
2020-05-21 00:07:53 +0800

    update bench

--- benchmarks/buffer.ts
+++ benchmarks/buffer.ts
@@ -1,45 +1,47 @@
+import { Buffer } from "../build/Buffer";
+
 function bufferFromArrayBuffer(number: number, times: number): Buffer {
-  let i = 0
-  let buf
+  let i = 0;
+  let buf;
   while (i < times) {
-    buf = Buffer.from(new ArrayBuffer(number))
-    i++
+    buf = Buffer.bufferFrom(new ArrayBuffer(number));
+    i++;
   }
-  return buf
+  return buf;
 }
 
 function bufferAlloc(number: number, times: number): Buffer {
-  let i = 0
-  let buf
+  let i = 0;
+  let buf;
   while (i < times) {
-    buf = Buffer.alloc(number)
-    i++
+    buf = Buffer.alloc(number);
+    i++;
   }
-  return buf
+  return buf;
 }
 
 function bench(number, times): void {
-  console.time(`bufferFromArrayBuffer number=${number} times=${times}`)
-  bufferFromArrayBuffer(number, times)
-  console.timeEnd(`bufferFromArrayBuffer number=${number} times=${times}`)
-  console.time(`bufferFromAllocBuffer number=${number} times=${times}`)
-  bufferAlloc(number, times)
-  console.timeEnd(`bufferFromAllocBuffer number=${number} times=${times}`)
-  console.log('')
+  console.time(`bufferFromArrayBuffer number=${number} times=${times}`);
+  bufferFromArrayBuffer(number, times);
+  console.timeEnd(`bufferFromArrayBuffer number=${number} times=${times}`);
+  console.time(`bufferFromAllocBuffer number=${number} times=${times}`);
+  bufferAlloc(number, times);
+  console.timeEnd(`bufferFromAllocBuffer number=${number} times=${times}`);
+  console.log("");
 }
 
 function main(): void {
-  bufferFromArrayBuffer(100, 100)
-  bufferAlloc(100, 100)
+  bufferFromArrayBuffer(100, 100);
+  bufferAlloc(100, 100);
 
-  bench(100, 10000)
-  bench(200, 10000)
-  bench(300, 10000)
-  bench(400, 10000)
-  bench(500, 10000)
-  bench(1000, 10000)
-  bench(2000, 10000)
-  bench(3000, 10000)
+  bench(100, 10000);
+  bench(200, 10000);
+  bench(300, 10000);
+  bench(400, 10000);
+  bench(500, 10000);
+  bench(1000, 10000);
+  bench(2000, 10000);
+  bench(3000, 10000);
 }
 
-main()
+main();
